8fe74ee7a3eaa21924aef0dbc1845dc28fd8ba5d
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-10-27 14:16:28 +0100

    Ammo Pick Up, and other changes
    Custom ammo pick up notifications, and an ability to customize it a bit

--- hud/lua/HUD/client/cl_hud.lua
+++ hud/lua/HUD/client/cl_hud.lua
@@ -6,6 +6,14 @@ surface.CreateFont("main2", {
     name = "Nachlieli CLM",
     size = 10
 })
+surface.CreateFont("pickUp", {
+    name = "Nachlieli CLM",
+    size = 15,
+})
+
+CreateClientConVar("easy_hud_always_visible", 1, true, false)
+CreateClientConVar("easy_hud_fade_time", 5, true, false)
+CreateClientConVar("easy_hud_hide_pickup", 0, true, false)
 
 local plyStartHP, plyOldHP, plyNewHP, plyStartArmor, plyOldArmor, plyNewArmor = 0, -1, -1, 0, -1, -1
 local HUDAlpha, HUDAmmoAlpha = 1, 1
@@ -22,7 +30,7 @@ hook.Add("PostDrawEffects", "findViewModelMuzzleForMWBsae", function()
     end
 end)
 
-// This is for default weapons, ArcCW and CW weapons
+// This is for default weapons, ArcCW and CW weapons (and anything else that just works)
 hook.Add("PostDrawViewModel", "findViewModelMuzzle", function(vm, ply, weapon)
     weaponClass = weapon:GetClass()
 
@@ -42,6 +50,8 @@ end)
 
 
 hook.Add("HUDPaintBackground", "drawHUD", function()
+    local scaleX = ScrW() / 1600 -- 
+    local scaleY = ScrH() / 900 -- It's divided by 1600 and 900 because I was creating it on that resolution.
     local ply = LocalPlayer()
 
     local plyHP = ply:Health()
@@ -52,6 +62,8 @@ hook.Add("HUDPaintBackground", "drawHUD", function()
 
     local lerpingPlayerHP = Lerp(math.ease.OutCubic(CurTime() - plyStartHP), plyOldHP, plyNewHP)
     local lerpingPlayerArmor = Lerp(math.ease.OutCubic(CurTime() - plyStartArmor), plyOldArmor, plyNewArmor)
+    local fadeTime = GetConVar("easy_hud_fade_time"):GetFloat()
+    local isAlwaysVisible = GetConVar("easy_hud_always_visible"):GetFloat()
 
     if lastActionDamage == -1 and lastActionAmmo == -1 then
         lastActionDamage = CurTime()
@@ -84,18 +96,9 @@ hook.Add("HUDPaintBackground", "drawHUD", function()
         plyNewArmor = plyArmor
     end
 
-    hook.Add("PlayerBindPress", "checkReload", function(ply, bind, pressed)
-        if bind == "+reload" and pressed then
-            if IsValid(ply:GetActiveWeapon()) and ply:GetActiveWeapon():IsWeapon() then
-                HUDAmmoAlpha = 255
-                lastActionAmmo = CurTime()
-            end
-        end
-    end)
-
     if ply:GetActiveWeapon():IsValid() then
         if muzzlePos == nil then
-            draw.SimpleTextOutlined(ply:GetAmmoCount(ply:GetActiveWeapon():GetPrimaryAmmoType()), "main", ScrW() * .9, ScrH() * .9, Color(196, 150, 110, 255), 0, 0, 1, Color(0, 0, 0, 255))
+            draw.SimpleTextOutlined(ply:GetAmmoCount(ply:GetActiveWeapon():GetPrimaryAmmoType()), "main", 1440 * scaleX, 810 * scaleY, Color(196, 150, 110, 255), 0, 0, 1, Color(0, 0, 0, 255))
         elseif ply:GetActiveWeapon():Clip1() != -1 and ply:GetActiveWeapon():GetPrimaryAmmoType() != nil then
             local plyWep = ply:GetActiveWeapon()
             local plyAmmo = plyWep:Clip1()
@@ -107,27 +110,41 @@ hook.Add("HUDPaintBackground", "drawHUD", function()
         end
     end
 
-    if CurTime() - lastActionDamage > 5 then
-        HUDAlpha = Lerp((CurTime() - lastActionDamage - 5), HUDAlpha, 1)
-    end
+    if isAlwaysVisible > 0 then
+        HUDAlpha = 255
+        HUDAmmoAlpha = 255
+    else
+        if CurTime() - lastActionDamage > fadeTime then
+            HUDAlpha = Lerp((CurTime() - lastActionDamage - fadeTime), HUDAlpha, 1)
+        end
 
-    if CurTime() - lastActionAmmo > 5 then
-        HUDAmmoAlpha = Lerp((CurTime() - lastActionAmmo - 5), HUDAmmoAlpha, 1)
+        if CurTime() - lastActionAmmo > fadeTime then
+            HUDAmmoAlpha = Lerp((CurTime() - lastActionAmmo - fadeTime), HUDAmmoAlpha, 1)
+        end
     end
 
     if plyHP - plyMaxHP > 0 then
-        draw.SimpleText("+" .. plyHP - plyMaxHP, "main2", (ScrW() * .03125) + 402, ScrH() * .927, Color(255, 255, 255, HUDAlpha), 0, 0)
+        draw.SimpleText("+" .. plyHP - plyMaxHP, "main2", 452 * scaleX, 834 * scaleY, Color(255, 255, 255, HUDAlpha), 0, 0)
     end
 
-    draw.RoundedBox(0, ScrW() * .03125, ScrH() * .933334, 400, 2, Color(0, 0, 0, HUDAlpha))
-    draw.RoundedBox(0, ScrW() * .03125, ScrH() * .933334, math.Clamp(400 * (lerpingPlayerHP / plyMaxHP), 0, 400), 2, Color(83, 160, 61, HUDAlpha))
+    draw.RoundedBox(0, 50 * scaleX, 840 * scaleY, 400 * scaleX, 2, Color(0, 0, 0, HUDAlpha))
+    draw.RoundedBox(0, 50 * scaleX, 840 * scaleY, (math.Clamp(400 * (lerpingPlayerHP / plyMaxHP), 0, 400)) * scaleX, 2, Color(83, 160, 61, HUDAlpha))
 
     if plyArmor - plyMaxArmor > 0 then
-        draw.SimpleText("+" .. plyArmor - plyMaxArmor, "main2", (ScrW() * .03125) + 402, ScrH() * .939, Color(255, 255, 255, HUDAlpha), 0, 0)
+        draw.SimpleText("+" .. plyArmor - plyMaxArmor, "main2", 452 * scaleX, 845 * scaleY, Color(255, 255, 255, HUDAlpha), 0, 0)
     end
 
-    draw.RoundedBox(0, ScrW() * .03125, ScrH() * .94445, 400, 2, Color(0, 0, 0, HUDAlpha))
-    draw.RoundedBox(0, ScrW() * .03125, ScrH() * .94445, math.Clamp(400 * (lerpingPlayerArmor / plyMaxArmor), 0, 400), 2, Color(67, 166, 198, HUDAlpha))
+    draw.RoundedBox(0, 50 * scaleX, 850 * scaleY, 400 * scaleX, 2, Color(0, 0, 0, HUDAlpha))
+    draw.RoundedBox(0, 50 * scaleX, 850 * scaleY, (math.Clamp(400 * (lerpingPlayerArmor / plyMaxArmor), 0, 400)) * scaleX, 2, Color(67, 166, 198, HUDAlpha))
+end)
+
+hook.Add("PlayerBindPress", "checkReload", function(ply, bind, pressed)
+    if bind == "+reload" and pressed then
+        if IsValid(ply:GetActiveWeapon()) and ply:GetActiveWeapon():IsWeapon() then
+            HUDAmmoAlpha = 255
+            lastActionAmmo = CurTime()
+        end
+    end
 end)
 
 // Hides default HUD
@@ -142,4 +159,95 @@ hook.Add( "HUDShouldDraw", "HideHUD", function( name )
     if ( hide[ name ] ) then
         return false
     end
+end)
+
+pickedUpItems = {}
+
+local function addPickUp(itemName)
+    local pickup = {}
+    pickup.time = CurTime()
+    pickup.name = itemName
+    pickup.holdtime = 5
+
+    table.insert(pickedUpItems, pickup)
+
+    return pickup
+end
+
+hook.Add("HUDWeaponPickedUp", "addWeaponToTable", function(wep)
+    if ( !IsValid( LocalPlayer() ) || !LocalPlayer():Alive() ) then return end
+    if ( !IsValid( wep ) ) then return end
+    if ( !isfunction( wep.GetPrintName ) ) then return end
+    if GetConVar("easy_hud_hide_pickup"):GetFloat() > 0 then return end
+
+    local pickup = addPickUp(wep:GetPrintName())
+    pickup.color = Color(255, 201, 46, 255)
+end)
+
+hook.Add("HUDItemPickedUp", "addItemToTable", function(item)
+    if ( !IsValid( LocalPlayer() ) || !LocalPlayer():Alive() ) then return end
+    if GetConVar("easy_hud_hide_pickup"):GetFloat() > 0 then return end
+
+    local pickup = addPickUp("#" .. item)
+    pickup.color = Color(181, 255, 181, 255)
+end)
+
+hook.Add("HUDAmmoPickedUp", "addAmmoToTable", function(item, amt)
+    if ( !IsValid( LocalPlayer() ) || !LocalPlayer():Alive() ) then return end
+    if GetConVar("easy_hud_hide_pickup"):GetFloat() > 0 then return end
+
+    local pickup = addPickUp("#" .. item .. "_ammo")
+    pickup.amount = tostring(amt)
+    pickup.color = Color(181, 201, 255, 255)
+end)
+
+local maxNotifications = 12 -- Maximum number of notifications to display at once
+
+hook.Add("HUDDrawPickupHistory", "drawHistory", function()
+    if (pickedUpItems == nil) then return end
+    if GetConVar("easy_hud_hide_pickup"):GetFloat() > 0 then return end
+
+    local scaleX = ScrW() / 1600 
+    local scaleY = ScrH() / 900 -- It's divided by 1600 and 900 because I was creating it on that resolution.
+
+    local totalHeight = 0
+    local displayedNotifications = 0 
+
+    for k, v in pairs(pickedUpItems) do
+        if (v.time + v.holdtime >= CurTime()) then
+            local delta = (v.time + v.holdtime) - CurTime()
+            delta = delta / v.holdtime
+
+            local alpha = 255
+
+            if (delta < 0.3) then
+                alpha = delta * (255 / 0.3)
+            end
+
+            surface.SetFont("pickUp") -- this is here so that surface.GetTextSize() works properly
+
+            -- funny box
+            surface.SetDrawColor(v.color.r, v.color.g, v.color.b, alpha)
+            surface.DrawRect(20 * scaleX, (totalHeight + 210) * scaleY, 2, 15)
+            -- name of thing
+            draw.SimpleTextOutlined(v.name, "pickUp", 25 * scaleX, (totalHeight + 210) * scaleY, Color(255, 255, 255, alpha), 0, 0, 0.5, Color(0, 0, 0, 255))
+
+            if v.amount then
+                draw.SimpleTextOutlined("x" .. v.amount, "pickUp", (30 + surface.GetTextSize(v.name)) * scaleX, (totalHeight + 210) * scaleY, Color(255, 255, 255, alpha), 0, 0, 0.5, Color(0, 0, 0, 255))
+            end 
+
+            totalHeight = totalHeight + 25
+            displayedNotifications = displayedNotifications + 1
+
+            if alpha == 0 then
+                pickedUpItems[k] = nil
+            end
+
+            if displayedNotifications >= maxNotifications then
+                break
+            end
+        end
+    end
+
+    return false
 end)
--- hud/lua/autorun/server/sv_hud.lua
+++
@@ -1,5 +0,0 @@
-util.AddNetworkString("sendInfo")
-
-hook.Add("PlayerHurt", "hudPlayerHurt", function(victim, attacker, healthRemaining, damageTaken)
-
-end)
